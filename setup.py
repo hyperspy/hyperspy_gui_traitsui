94aa864a8395046b05a7a3eb3f9370716fdb6e04
Francisco de la Peña <frandelapena@gmail.com>
2017-05-16 18:08:18 +0100

    Set traitsui min version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['hyperspy', 'traitsui'],
+    install_requires=['hyperspy', 'traitsui>=5.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
