1b4b3e6257594f21f08428f964635de562e005cb
Francisco de la Peña <frandelapena@gmail.com>
2018-08-31 17:47:31 +0200

    Release v1.1.0

--- hyperspy_gui_traitsui/__init__.py
+++ hyperspy_gui_traitsui/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with  HyperSpy.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "1.0.6"
+__version__ = "1.1.0"
 import logging
 
 import matplotlib
