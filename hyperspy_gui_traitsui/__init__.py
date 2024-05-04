e2d2073148f54736a48c13ab53425072d9af2acb
Eric Prestat <eric.prestat@gmail.com>
2024-05-04 07:46:53 +0100

    Fix getting version in editable install

--- hyperspy_gui_traitsui/__init__.py
+++ hyperspy_gui_traitsui/__init__.py
@@ -18,6 +18,7 @@
 
 
 import importlib
+from importlib.metadata import version
 import logging
 from pathlib import Path
 
@@ -27,16 +28,29 @@ from traits.etsconfig.api import ETSConfig
 from hyperspy.defaults_parser import preferences
 
 
-if Path(__file__).parent.parent.name == "site-packages":  # pragma: no cover
-    # Tested in the "Package & Test" workflow on GitHub CI
-    from importlib.metadata import version
+__version__ = version("hyperspy_gui_traitsui")
 
-    __version__ = version("hyperspy_gui_traitsui")
-else:
-    # Editable install
-    from setuptools_scm import get_version
+# For development version, `setuptools_scm` will be used at build time
+# to get the dev version, in case of missing vcs information (git archive,
+# shallow repository), the fallback version defined in pyproject.toml will
+# be used
 
-    __version__ = get_version(Path(__file__).parent.parent)
+# if we have a editable install from a git repository try to use
+# `setuptools_scm` to find a more accurate version:
+# `importlib.metadata` will provide the version at installation
+# time and for editable version this may be different
+
+# we only do that if we have enough git history, e.g. not shallow checkout
+_root = Path(__file__).resolve().parents[1]
+if (_root / ".git").exists() and not (_root / ".git/shallow").exists():
+    try:
+        # setuptools_scm may not be installed
+        from setuptools_scm import get_version
+
+        __version__ = get_version(_root)
+    except ImportError:  # pragma: no cover
+        # setuptools_scm not install, we keep the existing __version__
+        pass
 
 
 
